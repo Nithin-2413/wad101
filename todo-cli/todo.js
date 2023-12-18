05c4d8cd3c1211c685651159a68a320343022a46
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-19 00:14:16 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -6,35 +6,35 @@ const todoList = () => {
   };
 
   const markAsComplete = (index) => {
-    if (index >= 0 && index < all.length) {
-      all[index].completed = true;
-    } else {
-      console.error("Invalid index!");
-    }
+    // Marks a task as completed at a specific index in the list.
+    all[index].completed = true;
   };
 
   const overdue = () => {
+    // Filters and returns tasks that are overdue based on their due date.
     const today = new Date().toISOString().split("T")[0];
     return all.filter((item) => item.dueDate < today && !item.completed);
   };
 
   const dueToday = () => {
+    // Filters and returns tasks that are due today.
     const today = new Date().toISOString().split("T")[0];
     return all.filter((item) => item.dueDate === today && !item.completed);
   };
 
   const dueLater = () => {
+    // Filters and returns tasks that are due later.
     const today = new Date().toISOString().split("T")[0];
     return all.filter((item) => item.dueDate > today && !item.completed);
   };
 
   const toDisplayableList = (list) => {
-    const formattedList = list
-      .map((item) => {
-        const status = item.completed ? "[x]" : "[ ]";
-        return `${status} ${item.title} ${item.dueDate !== today ? item.dueDate : ''}`;
-      })
-      .join("\n");
+    // Formats the list of tasks for display, including completion status and due date.
+    const formattedList = list.map((item) => {
+      const status = item.completed ? "[x]" : "[ ]";
+      return `${status} ${item.title} ${item.dueDate !== today ? item.dueDate : ''}`;
+    }).join("\n");
+
     return formattedList;
   };
 
