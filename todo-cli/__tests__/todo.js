b5b5c663029408f9a837575a106437df9efd59e2
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-18 23:25:50 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -3,59 +3,71 @@ const todoList = require("./todo");
 describe("Todo List Test Suite", () => {
   let { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
-  beforeAll(() => {
-    const today = new Date();
-    const oneDay = 60 * 60 * 24 * 1000;
-    [
-      {
-        title: "Buy milk",
-        completed: false,
-        dueDate: new Date(today.getTime() - 2 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-      {
-        title: "Pay rent",
-        completed: false,
-        dueDate: new Date().toLocaleDateString("en-CA"),
-      },
-      {
-        title: "Submit assignment",
-        completed: false,
-        dueDate: new Date(today.getTime() + 2 * oneDay).toLocaleDateString(
-          "en-CA"
-        ),
-      },
-    ].forEach(add);
+  beforeEach(() => {
+    // Clear the list before each test
+    all = [];
   });
 
-  test("Should add a new todo", () => {
-    expect(all.length).toEqual(3);
-
+  test("Should add a todo", () => {
     add({
-      title: "A test item",
+      title: "Buy groceries",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date("2023-12-20").toLocaleDateString("en-CA"),
     });
 
-    expect(all.length).toEqual(4);
+    expect(all.length).toEqual(1);
   });
 
   test("Should mark a todo as complete", () => {
-    expect(all[0].completed).toEqual(false);
+    add({
+      title: "Buy groceries",
+      completed: false,
+      dueDate: new Date("2023-12-20").toLocaleDateString("en-CA"),
+    });
+
     markAsComplete(0);
     expect(all[0].completed).toEqual(true);
   });
 
   test("Should retrieve overdue items", () => {
-    expect(overdue().length).toEqual(1);
+    add({
+      title: "Buy groceries",
+      completed: false,
+      dueDate: new Date("2023-12-18").toLocaleDateString("en-CA"),
+    });
+
+    add({
+      title: "Pay bills",
+      completed: false,
+      dueDate: new Date("2023-12-17").toLocaleDateString("en-CA"),
+    });
+
+    expect(overdue().length).toEqual(2);
   });
 
   test("Should retrieve due today items", () => {
+    add({
+      title: "Buy groceries",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+
+    add({
+      title: "Pay bills",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+
     expect(dueToday().length).toEqual(2);
   });
 
   test("Should retrieve due later items", () => {
+    add({
+      title: "Submit assignment",
+      completed: false,
+      dueDate: new Date("2023-12-25").toLocaleDateString("en-CA"),
+    });
+
     expect(dueLater().length).toEqual(1);
   });
 });
