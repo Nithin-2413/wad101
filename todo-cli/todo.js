66c0c53477218efbea67a7c143f05300cfae1d39
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-18 23:32:52 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -11,7 +11,7 @@ const todoList = () => {
 
   const overdue = () => {
     return all.filter(
-      (item) => new Date(item.dueDate) < new Date()
+      (item) => new Date(item.dueDate) < new Date().toLocaleDateString("en-CA")
     );
   };
 
@@ -24,11 +24,21 @@ const todoList = () => {
 
   const dueLater = () => {
     return all.filter(
-      (item) => new Date(item.dueDate) > new Date()
+      (item) => new Date(item.dueDate) > new Date().toLocaleDateString("en-CA")
     );
   };
 
-  return { all, add, markAsComplete, overdue, dueToday, dueLater };
+  const toDisplayableList = () => {
+    return all.map((item) => {
+      return {
+        title: item.title,
+        completed: item.completed ? "Yes" : "No",
+        dueDate: item.dueDate,
+      };
+    });
+  };
+
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
 };
 
 module.exports = todoList;
