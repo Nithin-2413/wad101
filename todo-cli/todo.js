d60a867c74fe36be09c7b9fdd54d11ef431b9b69
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-19 00:13:27 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,45 +1,41 @@
-function todoList() {
-  const all = [];
+const todoList = () => {
+  let all = [];
 
   const add = (todoItem) => {
     all.push(todoItem);
   };
 
   const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
-
-  const dueToday = () => {
-    const today = new Date().toLocaleDateString("en-CA");
-    return all.filter(
-      (item) => item.dueDate === today
-    );
-    return all.filter((todo) => todo.dueDate === new Date().toISOString().split("T")[0]);
+    if (index >= 0 && index < all.length) {
+      all[index].completed = true;
+    } else {
+      console.error("Invalid index!");
+    }
   };
 
   const overdue = () => {
     const today = new Date().toISOString().split("T")[0];
-    return all.filter((todo) => todo.dueDate < today);
+    return all.filter((item) => item.dueDate < today && !item.completed);
   };
 
   const dueToday = () => {
     const today = new Date().toISOString().split("T")[0];
-    return all.filter((todo) => isDueToday(todo.dueDate));
+    return all.filter((item) => item.dueDate === today && !item.completed);
   };
 
   const dueLater = () => {
     const today = new Date().toISOString().split("T")[0];
-    return all.filter((todo) => todo.dueDate > today);
+    return all.filter((item) => item.dueDate > today && !item.completed);
   };
 
-  const toDisplayableList = () => {
-    return all
-      .map((todo) => {
-        const dueDate = isDueToday(todo.dueDate) ? "" : todo.dueDate;
-        const status = todo.completed ? "[x]" : "[ ]";
-        return `${status} ${todo.title} ${dueDate}`;
+  const toDisplayableList = (list) => {
+    const formattedList = list
+      .map((item) => {
+        const status = item.completed ? "[x]" : "[ ]";
+        return `${status} ${item.title} ${item.dueDate !== today ? item.dueDate : ''}`;
       })
       .join("\n");
+    return formattedList;
   };
 
   return {
@@ -51,6 +47,6 @@ function todoList() {
     dueLater,
     toDisplayableList,
   };
-}
+};
 
 module.exports = todoList;
