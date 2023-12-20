d4713fe3fac411f2decc4e4694df76ae4513d37b
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-20 21:49:39 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,76 +1,95 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
 
-module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    static async overdue() {
-      try {
-        const overdueTasks = await Todo.findAll({
-          where: {
-            dueDate: { [sequelize.Op.lt]: new Date() },
-          },
-        });
-        return overdueTasks;
-      } catch (error) {
-        throw new Error('Unable to fetch overdue tasks');
-      }
-    }
+const { Model, Op } = require("sequelize");
 
-    static async dueToday() {
-      try {
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        const dueTodayTasks = await Todo.findAll({
-          where: {
-            dueDate: today,
-          },
-        });
-        return dueTodayTasks;
-      } catch (error) {
-        throw new Error('Unable to fetch tasks due today');
-      }
-    }
-
-    static async dueLater() {
-      try {
-        const dueLaterTasks = await Todo.findAll({
-          where: {
-            dueDate: { [sequelize.Op.gt]: new Date() },
-          },
-        });
-        return dueLaterTasks;
-      } catch (error) {
-        throw new Error('Unable to fetch tasks due later');
-      }
-    }
-
-    static async markAsComplete(id) {
-      try {
-        const todo = await Todo.findByPk(id);
-        if (todo) {
-          todo.completed = true;
-          await todo.save();
-          return todo;
-        }
-        throw new Error('Todo not found');
-      } catch (error) {
-        throw new Error('Unable to mark todo as complete');
-      }
-    }
-
-    displayableString() {
-      // Implement displayableString method to return formatted strings for different scenarios
-    }
+class CustomTodoModel extends Model {
+  static async addTask(params) {
+    return await CustomTodoModel.create(params);
   }
 
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  static async showList() {
+    console.log("My Todo list \n");
 
-  return Todo;
+    console.log("Overdue");
+    console.log(
+      (await CustomTodoModel.overdue())
+        .map((todo) => {
+          return todo.displayableString();
+        })
+        .join("\n")
+    );
+    console.log("\n");
+
+    console.log("Due Today");
+    console.log(
+      (await CustomTodoModel.dueToday())
+        .map((todo) => todo.displayableString())
+        .join("\n")
+    );
+    console.log("\n");
+
+    console.log("Due Later");
+    console.log(
+      (await CustomTodoModel.dueLater())
+        .map((todo) => todo.displayableString())
+        .join("\n")
+    );
+  }
+
+  static async overdue() {
+    return await CustomTodoModel.findAll({
+      where: {
+        dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
+      },
+    });
+  }
+
+  static async dueToday() {
+    return await CustomTodoModel.findAll({
+      where: {
+        dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
+      },
+    });
+  }
+
+  static async dueLater() {
+    return await CustomTodoModel.findAll({
+      where: {
+        dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
+      },
+    });
+  }
+
+  static async markAsComplete(id) {
+    await CustomTodoModel.update(
+      { completed: true },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+  }
+
+  displayableString() {
+    let checkbox = this.completed ? "[x]" : "[ ]";
+    return `${this.id}. ${checkbox} ${this.title} ${
+      this.dueDate == new Date().toLocaleDateString("en-CA") ? "" : this.dueDate
+    }`.trim();
+  }
+}
+
+module.exports = (sequelize, DataTypes) => {
+  CustomTodoModel.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "CustomTodoModel",
+    }
+  );
+  return CustomTodoModel;
 };
