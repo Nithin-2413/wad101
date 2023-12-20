fad62b7e79e2226e0468f5a8a3d56b331aba4c82
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-20 21:53:48 +0530

    Update connectDB.js

--- todo-cli/connectDB.js
+++ todo-cli/connectDB.js
@@ -1,1 +1,33 @@
+const Sequelize = require("sequelize");
+
+const databaseConfig = {
+  database: "unique_todo_db",
+  username: "unique_user",
+  password: "secure_password",
+  options: {
+    host: "localhost",
+    dialect: "postgres",
+  },
+};
+
+const sequelize = new Sequelize(
+  databaseConfig.database,
+  databaseConfig.username,
+  databaseConfig.password,
+  databaseConfig.options
+);
+
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = {
+  connect,
+  sequelize,
+};
 
